b02de1c5bb3a1ece7ed23aca0c5ec064b521f589
Ascyus <halabak13@gmail.com>
2026-04-06 03:22:12 -0400

    feat: add file logging for System Pulse snapshots

--- System_Pulse.py
+++ System_Pulse.py
@@ -6,20 +6,29 @@ import time
 is_windows = os.name == 'nt'
 root_path = 'C:\\' if is_windows else '/'
 clear_command = 'cls' if is_windows else 'clear'
+log_file_path = "log.txt"
 
-def print_system_usage():
+def get_system_usage_text():
 
     # Data gathering behaviour (to be migrated into a separate function)
     cpu_usage = psutil.cpu_percent(interval = 0.5)
-    memory_usage = psutil.virtual_memory()
-    disk_usage = psutil.disk_usage(root_path)
+    memory_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage(root_path).percent
 
-    print(f"CPU Usage: {cpu_usage}%")
-    print(f"Memory Usage: {memory_usage.percent}%")
-    print(f"Disk Usage: {disk_usage.percent}%")
-
+    return (
+        f"CPU Usage: {cpu_usage}%\n"
+        f"Memory Usage: {memory_usage}%\n"
+        f"Disk Usage: {disk_usage}%"
+    )
+        
 while True:
 
     subprocess.run(clear_command, shell = True)
-    print_system_usage()
+
+    usage_text = get_system_usage_text()
+    print(usage_text)
+
+    with open(file = log_file_path, mode = "a") as file:
+        file.write(usage_text + "\n\n")
+
     time.sleep(2)
