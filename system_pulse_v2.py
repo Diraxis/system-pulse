39cdfb25d0f8dd663fb25559373002b62305e74f
Ascyus <halabak13@gmail.com>
2026-04-14 05:47:21 -0400

    feat: handle clean shutdown on KeyboardInterrupt

--- system_pulse_v2.py
+++ system_pulse_v2.py
@@ -12,44 +12,47 @@ clear_terminal = 'cls' if is_windows else 'clear'
 log_file_path  = "log.txt"
 
 # program-specific functions
-def system_usage(): # intended to gather three usage states and remember their data as variables
+def get_system_usage(): # gathers CPU, memory, and disk usage values
     
     cpu_usage    = psutil.cpu_percent(interval = 0.5)
     memory_usage = psutil.virtual_memory().percent
     disk_usage   = psutil.disk_usage(root_path).percent
     
-    return(cpu_usage, memory_usage, disk_usage) # function returns the variables' data when called
+    return cpu_usage, memory_usage, disk_usage # function returns usage values when called
 
-def format_usage(cpu_usage, memory_usage, disk_usage): # This function imports the return data of "system_usage()" to be formatted
+def format_usage(cpu_usage, memory_usage, disk_usage): # imports the return values of "get_system_usage()" to be formatted
 
-    return(
+    return (
         f"CPU Usage: {cpu_usage}%\n"
         f"Memory Usage: {memory_usage}%\n"
         f"Disk Usage: {disk_usage}%"
     ) # function returns formatted strings (f-strings) representing system usage data
 
 # execution program
-while True:
+try: # code that might fail 
+    while True:
     
-    # OS-independent command
-    subprocess.run(clear_terminal, shell = True)
-    
-    # real-time f-string variable
-    timestamp_str = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+        # OS-independent command
+        subprocess.run(clear_terminal, shell = True)
+        
+        # real-time f-string variable
+        timestamp_str = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
-    # refresh of variables' data and formats it and remembers via a single variable
-    cpu_usage, memory_usage, disk_usage = system_usage()
-    usage_str = format_usage(cpu_usage, memory_usage, disk_usage)
+        # data refreshes and data is formatted and held in a variable
+        cpu_usage, memory_usage, disk_usage = get_system_usage()
+        usage_str = format_usage(cpu_usage, memory_usage, disk_usage)
 
-    # formats timestamp and system usage as one package
-    entry_log = f"{timestamp_str}\n{usage_str}\n\n"
+        # formats timestamp and system usage as one package
+        entry_log = f"{timestamp_str}\n{usage_str}\n\n"
 
-    # terminal and log outputs
-    print(entry_log)
-    print(f"Writing to {log_file_path}")
+        # terminal and log outputs
+        print(entry_log)
+        print(f"Writing to {log_file_path}")
 
-    with open (file = log_file_path, mode = 'a') as file:
-        file.write(entry_log)
+        with open(file = log_file_path, mode = 'a') as file:
+            file.write(entry_log)
 
-    # loop execution delay
-    time.sleep(2.5)
+        # loop execution delay
+        time.sleep(2.5)
+except KeyboardInterrupt: # exception handling for a specific code failure. (Ctrl+C)
+    print("\n[System Pulse v2]: Program ended by user.\n")
