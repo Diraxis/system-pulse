b6d5418ca85e2555f72ca2c5563bc9f06fcc5e59
Ascyus <halabak13@gmail.com>
2026-04-22 18:58:58 -0400

    refactor: convert output to single-line structured log format

--- system_pulse_v2.py
+++ system_pulse_v2.py
@@ -21,36 +21,37 @@ def get_system_usage(): # gathers CPU, memory, and disk usage values
 
 def format_usage(stats): # imports the return values of "get_system_usage()" via the 'stats' variables to be formatted
     return (
-        f"CPU Usage    : {stats['cpu']}%\n"
-        f"Memory Usage : {stats['memory']}%\n"
-        f"Disk Usage   : {stats['disk']}%"
+        f"CPU Usage: {stats['cpu']}% | "
+        f"Memory Usage: {stats['memory']}% | "
+        f"Disk Usage: {stats['disk']}%"
     ) # function returns formatted strings (f-strings) representing system usage data
 
 # execution program
-try: # main monitoring loop
+# main monitoring loop
+try: 
     while True: 
     
         # OS-independent command
         subprocess.run(clear_terminal, shell = True)
         
         # real-time f-string variable
-        timestamp_str = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+        entry_time = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
         # data refreshes and data is formatted and held in a variable
         stats = get_system_usage()
         usage_str = format_usage(stats)
 
         # formats timestamp and system usage as one package
-        entry_log = f"{timestamp_str}\n{usage_str}"
+        entry_log = f"{entry_time} | {usage_str}"
 
         # terminal and log outputs
         print(entry_log)
 
         with open(file = log_file_path, mode = 'a') as file:
-            file.write(entry_log + "\n\n")
+            file.write(entry_log + "\n")
 
         # loop execution delay
         time.sleep(2.5)
 
 except KeyboardInterrupt: # handle user interrupt (Ctrl+C) for clean exit
-    print("\n[System Pulse v2]: Program ended by user.\n")
+    print("[System Pulse v2]: Program ended by user.\n")
